c98e995ce18521c2d7cfc588ebb0b496174f9554
Bernardo <bernardobrasileirod@gmail.com>
2026-09-14 15:16:27 -0300

    Delete aula02 directory

--- aula02/Conta Cliques/script.js
+++
@@ -1,8 +0,0 @@
-const botao = document.querySelector('#botao');
-const numero = document.querySelector('#numero');
-let contador = 0;
-
-botao.addEventListener('click', function () {
-    contador++;
-    numero.textContent = contador;
-})
