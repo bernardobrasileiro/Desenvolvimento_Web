591bc8e8568296438caa38a8de5fb73fb405d2d6
Bernardo <bernardobrasileirod@gmail.com>
2026-09-18 14:24:22 -0300

    Update prompts for user input in Q07.js

--- Lista de Exercícios/Q07.js
+++ Lista de Exercícios/Q07.js
@@ -1,6 +1,6 @@
-let nome = prompt('Qual o seu nome?');
-let idade = Number(prompt('Quantos anos você tem'));
-let curso = prompt('Qual o seu curso?');
+let nome = prompt('Insira seu nome: ');
+let idade = Number(prompt('Insira sua idade: '));
+let curso = prompt('Insira seu curso: ');
 
 let resposta = confirm('As informações acima estão corretas?');
 
